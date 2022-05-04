87917fed1816aabeddb138d9ec069c51f8252584
tristanbasb <73985102+tristanbasb@users.noreply.github.com>
2022-05-04 13:07:39 +0100

    Update installsave.sh

--- installsave.sh
+++ installsave.sh
@@ -10,10 +10,8 @@ cd /save
 ####gdrive####
 #Installation de gdrive qui permet d'upload et de télécharger de linux vers google drive
 wget https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_linux_386.tar.gz >/dev/null 2>&1
-
 #Décompresse gdrive
 tar -xvf gdrive_2.1.1_linux_386.tar.gz >/dev/null 2>&1
-
 #Supprime le fichier compresser de gdrive
 rm -r gdrive_2.1.1_linux_386.tar.gz
 
@@ -22,16 +20,12 @@ rm -r gdrive_2.1.1_linux_386.tar.gz
 ####pfmotion_curl.sh####
 #Intallation de pfmotion_curl.sh qui permet de créer une backup de pfsense
 git clone https://github.com/blogmotion/bm-backup-pfsense.git >/dev/null 2>&1
-
 #Copie colle le fichier pfmotion_curl.sh
 cp /save/bm-backup-pfsense/pfmotion_curl.sh /save/pfmotion_curl.sh
-
 #Supprime le fichier bm-backup-pfsense
 rm -r /save/bm-backup-pfsense
-
 #Installation de curl
 apt install curl -y >/dev/null 2>&1
-
 #Création d'un dossier conf_backup pour mettre les backup
 mkdir /save/conf_backup
 
@@ -40,42 +34,56 @@ mkdir /save/conf_backup
 ####Initialisation####
 #Vide le terminal
 clear
-
 #Informe l'utilisateur
 echo "Veuillez copier le lien en dessous et le coller dans votre naviguateur. Ensuite utilisez votre compte et autoriser l'utilisatation, une fois que c'est fait copier le code d'autorisation"
 echo " "
-
 #Permet de récuperer les informations du compte google de l'utilisateur
 /save/gdrive about
-
 echo " "
 
+
+
+####Changer adresse ip#####
 #Informe l'utilisateur
 echo "Veuillez maintenant renseigner l'addresse ip de votre pfsense :"
-
 #Permet de récupere l'information donner par l'utilisateur
 read addIp
-
+#Permet de modifier l'adresse ip de la ligne 14 par l'adresse choisi par l'utilisateur 
 sed -i '14 s/192.168.12.34/'$addIp'/' /save/pfmotion_curl.sh
+
+####Changer le nom d'utilisateur####
+#Informe l'utilisateur
+echo "Veuillez renseigner le nom de l'utilisateur que vous avez créez pour les backup :"
+#Permet de récupere l'information donner par l'utilisateur
+read user
+#Permet de modifier le nom d'utilisateur de la ligne 17 
+sed -i '17 s/admin/'$user'/' /save/pfmotion_curl.sh
+
+####Changer le mot de passe####
+#Informe l'utilisateur
+echo "Veuillez renseigner le mot de passe de l'utilisateur que vous avez créez pour les backup :"
+#Permet de récupere l'information donner par l'utilisateur
+read mdp
+#Permet de modifier le mot de passe de la ligne 18 
+sed -i '18 s/admin/'$mdp'/' /save/pfmotion_curl.sh
+
 
 
 ####autogdrive####
 #Copie colle autogdrive.sh vers /save/
-cp /opt/autogdrive.sh /save/autogdrive.sh
+cp /opt/test/autogdrive.sh /save/autogdrive.sh
+
 
 
 ####crontab####
 #Supprime le cron existant
 crontab -l > moncron >/dev/null 2>&1
-
 #Permet de mettre la ligne suivante dans le cron
 echo 
 "0 10 * * * /save/pfmotion_curl.sh
 0 15 * * * /save/autogdrive.sh
-" >> moncron
-
+" >> moncron >/dev/null 2>&1
 #Installer un nouveau fichier moncron 
 crontab moncron
-
 #Supprime le fichier moncron
 rm moncron
