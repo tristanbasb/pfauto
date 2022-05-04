ad0bdcba15c51e90a49ec38f4c303b1a6438a0e9
tristanbasb <73985102+tristanbasb@users.noreply.github.com>
2022-05-04 10:50:22 +0100

    Create installsave.sh

---
+++ installsave.sh
@@ -0,0 +1,33 @@
+#!bin/bash
+
+#Création d'un dossier save
+mkdir /save
+
+cd /save
+
+
+
+####gdrive####
+#Installation de gdrive qui permet d'upload et de télécharger de linux vers google drive
+wget https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_linux_386.tar.gz
+
+#Décompresse gdrive
+tar -xvf gdrive_2.1.1_linux_386.tar.gz
+
+#Supprime le fichier compresser de gdrive
+rm -r gdrive_2.1.1_linux_386.tar.gz
+
+
+
+####pfmotion_curl.sh####
+#Intallation de pfmotion_curl.sh qui permet de créer une backup de pfsense
+git clone https://github.com/blogmotion/bm-backup-pfsense.git
+
+#Copie colle le fichier pfmotion_curl.sh
+cp /save/bm-backup-pfsense/pfmotion_curl.sh /save/pfmotion_curl.sh
+
+#Supprime le fichier bm-backup-pfsense
+rm -r /save/bm-backup-pfsense
+
+#Installation de curl
+apt install curl -y
