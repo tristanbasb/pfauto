e54548e9cebc9ba68bbad837399d601e16246495
tristanbasb <73985102+tristanbasb@users.noreply.github.com>
2022-05-04 12:00:33 +0100

    to gdrve

--- installsave.sh
+++ installsave.sh
@@ -35,15 +35,23 @@ apt install curl -y >/dev/null 2>&1
 #Création d'un dossier conf_backup pour mettre les backup
 mkdir /save/conf_backup
 
+
+
+####crontab####
 #Supprime le cron existant
 crontab -l > moncron
 
 #Permet de mettre la ligne suivante dans le cron
-echo "1 * * * * /save/pfmotion_curl.sh" >> moncron
+echo 
+"0 10 * * * /save/pfmotion_curl.sh
+ 0 15 * * * /save/autogdrive.sh
+" >> moncron
 
 #Installer un nouveau fichier moncron 
 crontab moncron
 
 #Supprime le fichier moncron
 rm moncron
+
+
 
