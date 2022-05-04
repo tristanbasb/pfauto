ef05084b0834f05d9483d821f933ebf3d5369600
tristanbasb <73985102+tristanbasb@users.noreply.github.com>
2022-05-04 11:04:33 +0100

    Update installsave.sh

--- installsave.sh
+++ installsave.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
 #Création d'un dossier save
-mkdir /save
+mkdir /save >/dev/null 2>&1
 
 cd /save
 
@@ -9,29 +9,29 @@ cd /save
 
 ####gdrive####
 #Installation de gdrive qui permet d'upload et de télécharger de linux vers google drive
-wget https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_linux_386.tar.gz
+wget https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_linux_386.tar.gz >/dev/null 2>&1
 
 #Décompresse gdrive
-tar -xvf gdrive_2.1.1_linux_386.tar.gz
+tar -xvf gdrive_2.1.1_linux_386.tar.gz >/dev/null 2>&1
 
 #Supprime le fichier compresser de gdrive
-rm -r gdrive_2.1.1_linux_386.tar.gz
+rm -r gdrive_2.1.1_linux_386.tar.gz >/dev/null 2>&1
 
 
 
 ####pfmotion_curl.sh####
 #Intallation de pfmotion_curl.sh qui permet de créer une backup de pfsense
-git clone https://github.com/blogmotion/bm-backup-pfsense.git
+git clone https://github.com/blogmotion/bm-backup-pfsense.git >/dev/null 2>&1
 
 #Copie colle le fichier pfmotion_curl.sh
-cp /save/bm-backup-pfsense/pfmotion_curl.sh /save/pfmotion_curl.sh
+cp /save/bm-backup-pfsense/pfmotion_curl.sh /save/pfmotion_curl.sh >/dev/null 2>&1
 
 #Supprime le fichier bm-backup-pfsense
-rm -r /save/bm-backup-pfsense
+rm -r /save/bm-backup-pfsense >/dev/null 2>&1
 
 #Installation de curl
-apt install curl -y
+apt install curl -y >/dev/null 2>&1
 
 #Création d'un dossier conf_backup pour mettre les backup
-mkdir /save/conf_backup
+mkdir /save/conf_backup >/dev/null 2>&1
 
