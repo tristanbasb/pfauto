1b5e58729fbdae8d053fb5247647d76891395545
tristanbasb <73985102+tristanbasb@users.noreply.github.com>
2022-05-05 13:04:36 +0100

    Update autogdrive.sh

--- autogdrive.sh
+++ autogdrive.sh
@@ -1,9 +1,11 @@
 #!bin/bash
 
+#Permet de récuperer la date et la mettre dans un variable
 d=$(date +%Y-%m-%d-%H)
 
+#Permet de récuperer la premiere ligne du fichier iddossier
 ligne=1
 id=`sed -n $ligne'p' /save/iddossier`
 
-
+#Upload le backup vers le dossier parent sur google drive
 /save/gdrive upload -p "$id" /save/conf_backup/config-"$d".xml
